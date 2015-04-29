bd60a5363cc7e740731c428aa4c6cf710e5ea50d
dennislixin <dennislixin@gmail.com>
2015-04-30 01:06:43 +0900

    added remove method

--- BSTTest.java
+++ BSTTest.java
@@ -9,6 +9,34 @@ public class BSTTest{
     bst.insert(5);
     bst.insert(10);
     System.out.println(bst);
+
+    bst.remove(1);
+    System.out.println("remove 1");
+    System.out.println(bst);
+
+    bst.remove(5);
+    System.out.println("remove 5");
+    System.out.println(bst);
+
+    bst.remove(6);
+    System.out.println("remove 6");
+    System.out.println(bst);
+
+    bst.remove(10);
+    System.out.println("remove 10");
+    System.out.println(bst);
+
+    bst.remove(9);
+    System.out.println("remove 9");
+    System.out.println(bst);
+
+    bst.remove(3);
+    System.out.println("remove 3");
+    System.out.println(bst);
+
+    bst.remove(10);
+    System.out.println("remove 10");
+    System.out.println(bst);
   }
 }
 
@@ -29,21 +57,22 @@ class BST <E extends Comparable<? super E>>{
       return true;
     }
 
-    Node<E> next = head;
+    Node<E> child = head;
     Node<E> cur = null;
-    boolean isLeft = false;
+    boolean isLeftChildNull = false;
 
-    while(next != null){
-      cur = next;
+    while(child != null){
+      cur = child;
       if(value.compareTo(cur.getValue()) <= 0){
-        next = cur.getLeft();
-        isLeft = next == null;
+        child = cur.getLeft();
+        isLeftChildNull = true;
       } else {
-        next = cur.getRight();
+        child = cur.getRight();
+        isLeftChildNull = false;
       }
     }
 
-    if(isLeft){
+    if(isLeftChildNull){
       cur.setLeft(newNode);
     } else {
       cur.setRight(newNode);
@@ -52,8 +81,71 @@ class BST <E extends Comparable<? super E>>{
     return true;
   }
 
+  public boolean remove(E value){
+    if(value == null || head == null){
+      return false;
+    }
+
+    Node<E> parent = null;
+    Node<E> cur = head;
+    boolean isCurLeftChild = false;
+    while(cur != null && cur.getValue().compareTo(value) != 0){
+      parent = cur;
+      if(value.compareTo(cur.getValue()) < 0){
+        cur = cur.getLeft();
+        isCurLeftChild = true;
+      } else {
+        cur = cur.getRight();
+        isCurLeftChild = false;
+      }
+    }
+
+    Node<E> left = cur.getLeft();
+    Node<E> right = cur.getRight();
+
+    if(left == null || right == null){
+      Node<E> newChild;
+      if(left != null){
+        newChild = left;
+      } else{
+        newChild = right;
+      }
+      if(cur == head){
+        head = newChild;
+      } else {
+        if(isCurLeftChild){
+          parent.setLeft(newChild);
+        } else {
+          parent.setRight(newChild);
+        }
+      }
+    } else{
+      Node<E> successorOfCur = cur.getRight();
+      Node<E> parentOfSuccessor = cur;
+      while(successorOfCur.getLeft() != null){
+        parentOfSuccessor = successorOfCur;
+        successorOfCur = successorOfCur.getLeft();
+      }
+
+      cur.setValue(successorOfCur.getValue());
+
+      if(cur == parentOfSuccessor){
+        cur.setRight(successorOfCur.getRight());
+      } else {
+        parentOfSuccessor.setLeft(successorOfCur.getRight());
+      }
+    }
+
+    return true;
+  }
+
   public String toString(){
-    return head.toString();
+    if(head != null){
+      return head.toString();
+    } else {
+      return "BST is empty";
+    }
+
   }
 }
 
@@ -80,6 +172,10 @@ class Node <E extends Comparable<? super E>>{
 
   public void setRight(Node<E> newRight){
     right = newRight;
+  }
+
+  public void setValue(E value){
+    this.value = value;
   }
 
   public E getValue(){
